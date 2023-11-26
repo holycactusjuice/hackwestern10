392b02b82ecf6572b54c9b68b996bab9723c122a
Patrick He <77468352+he-patrick@users.noreply.github.com>
2023-11-26 10:50:42 -0500

    final

--- client/src/app/components/InterpretedText.tsx
+++ client/src/app/components/InterpretedText.tsx
@@ -1,19 +1,34 @@
 'use client';
-import {playfair} from '../fonts'
+
+import React, { useState } from 'react';
+import { playfair } from '../fonts';
 import ConvertDropdown from './ConvertDropdown';
+import Translator from "../../API/Translator";
 
 interface InterpretedTextProps {
   text: string;
 }
 
 const InterpretedText: React.FC<InterpretedTextProps> = ({ text }) => {
-  return <div> 
+  const [showTranslator, setShowTranslator] = useState<boolean>(false); // Specify type for useState
+
+  const handleClick = () => { // Use arrow function for consistency
+    setShowTranslator(true);
+  }
+
+  return (
+    <div> 
       <div className="outline outline-2 ml-4 pl-8 pr-8 pt-4 rounded-lg ring-blue-500 shadow-md outline-transparent overflow-y-auto h-64"> 
-      <h1 className={`pb-4 text-xl ${playfair.className}`}>Translated Text</h1>
-      <p className="text-m">{text}</p>
+        <h1 className={`pb-4 text-xl ${playfair.className}`}>Translated Text</h1>
+        <p className="text-m">{text}</p>
       </div>
-      <ConvertDropdown></ConvertDropdown>
+      <ConvertDropdown />
+
+      <button onClick={handleClick}>Translate</button>
+
+      {showTranslator && <Translator text={text} />} {/* Conditionally render Translator */}
     </div>
+  );
 }
 
 export default InterpretedText;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,16 +2,15 @@ import Camera from "./components/Camera"
 import React, {useRef, useState, useEffect} from "react";
 import InterpretedText from "./components/InterpretedText";
 import { exec, spawn, fork } from "child_process";
-import Translator from "../API/Translator";
 
-exec("echo hello", (err, stdout, stderr) => {
-  if (err) {
-    console.error(`exec error: ${err}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stdout: ${stderr}`);
-})
+// exec("echo hello", (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(`exec error: ${err}`);
+//     return;
+//   }
+//   console.log(`stdout: ${stdout}`);
+//   console.log(`stdout: ${stderr}`);
+// })
 
 const Home = () => {
   return (
@@ -19,13 +18,12 @@ const Home = () => {
       <main className="container mx-auto p-4">
         <h1 className="text-4xl font-bold mb-4">tranSLate</h1>
         <p className="text-lg">A way to communicate internationally through sign language.</p>
-        <Translator text="hi"/>
         <div className="m-8 grid grid-cols-2">
           <div>
             <Camera />
           </div>
           <div>
-            <InterpretedText text="dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajklajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajklajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajklajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajklajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkl jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjasld ljdfajkldf jakd fslkjd fkl adfskjldfsjl ljdfas  lkjadflj dsaj dsfjka kjlsda"/>
+            <InterpretedText text="Hello there!"/>
           </div>
         </div>
 
