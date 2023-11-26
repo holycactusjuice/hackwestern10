5b37f7ff00ce86286993f50143ed6712974d6e25
areuie <63759859+areuie@users.noreply.github.com>
2023-11-25 20:51:48 -0500

    added client and server folders

--- client/src/app/components/InterpretedText.tsx
+++ client/src/app/components/InterpretedText.tsx
@@ -2,8 +2,6 @@
 import {playfair} from '../fonts'
 import ConvertDropdown from './ConvertDropdown';
 
-
-
 const InterpretedText = (props) => {
   return <div> 
       <div className="outline outline-2 ml-4 pl-8 pr-8 pt-4 rounded-lg ring-blue-500 shadow-md outline-transparent overflow-y-auto h-64"> 
