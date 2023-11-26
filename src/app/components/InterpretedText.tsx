217faf678320eea024c4fd06a5b0684f7d601f9b
areuie <63759859+areuie@users.noreply.github.com>
2023-11-25 19:08:42 -0500

    added styling and font imports

--- src/app/components/InterpretedText.tsx
+++ src/app/components/InterpretedText.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 const InterpretedText = (props) => {
-  return <div className="outline outline-2 ml-4 rounded-lg ring-blue-500 shadow-md outline-transparent overflow-y-auto h-64"> 
-      <h1>Translated Text</h1>
+  return <div className="outline outline-2 ml-4 pl-8 pr-8 pt-4 rounded-lg ring-blue-500 shadow-md outline-transparent overflow-y-auto h-64"> 
+      <h1 className="pb-4">Translated Text</h1>
       <p>{props.text}</p>
     </div>
 }
